21b42aa675d3c11fc14cbaa8ca95b0e85d90a938
lseonyong <qnfto7898@naver.com>
2022-02-18 13:37:41 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="app">
+    <v-btn color="success">Success</v-btn>
+    <v-btn color="error">Error</v-btn>
+    <v-btn color="warning">Warning</v-btn>
+    <v-btn color="info">Info</v-btn>
+    <Header />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{Header},
+  data() {
+    return{
+      
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+      <h1>이선용</h1>
+      <img :src="btn_close" alt="닫기">
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            btn_close:require("../assets/icon-close.png")
+        }
+    }
+}
+</script>
+
+<style>
+    .header{
+        display:flex;
+        justify-content: space-between;
+        align-content: center;
+    }
+</style>
